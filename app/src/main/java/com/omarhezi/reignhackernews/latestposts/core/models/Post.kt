407cb2d915c04c3af4a682b1887b0e095357bfb0
Omar Hezi <omar.hezi@mediamonks.com>
2020-10-15 00:30:59 -0300

    removed Moshi dependency from core model

--- app/src/main/java/com/omarhezi/reignhackernews/latestposts/core/models/Post.kt
+++ app/src/main/java/com/omarhezi/reignhackernews/latestposts/core/models/Post.kt
@@ -1,27 +1,11 @@
 package com.omarhezi.reignhackernews.latestposts.core.models
 
-import com.squareup.moshi.Json
-
 data class Post(
-
-	@Json(name="author")
 	val author: String? = null,
-
-	@Json(name="story_id")
 	val storyId: Int? = null,
-
-	@Json(name="created_at_i")
 	val createdAt: String? = null,
-
-	@Json(name="title")
 	val title: String? = null,
-
-	@Json(name="url")
 	val url: String? = null,
-
-	@Json(name="story_title")
 	val storyTitle: String? = null,
-
-	@Json(name="story_url")
 	val storyUrl: String? = null
 )
